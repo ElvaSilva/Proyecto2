c3bfebd42b7e1d4f1c4dc2f261178d876c89c9c2
jecasais <casais.elohym@correo.unimet.edu.ve>
2025-07-06 23:03:20 -0400

    Add files via upload

--- ArbolBinBusq.java
+++ ArbolBinBusq.java
@@ -20,10 +20,7 @@ public class ArbolBinBusq {
     }
     
     public boolean IsEmpty(){
-        if(this.raiz == null){
-            return true;
-        }
-        return false;
+        return this.raiz == null;
     }
     
     public void insertar(Nodo aux){
@@ -32,10 +29,47 @@ public class ArbolBinBusq {
         }else{
             Nodo aux2 = new Nodo();
             aux2 = this.raiz;
-            while(aux2.hIzq != null){
-                if(aux2.valor > aux.valor){
+            while(aux2 != null){
+                if(aux.valor < aux2.valor){
+                    if(aux2.hIzq != null){
+                        aux2 = aux2.hIzq;
+                    }
+                    aux2.hIzq = aux;
+                }
+                if(aux.valor > aux2.valor){
+                    if(aux2.hDer != null){
+                        aux2 = aux2.hDer;
+                    }
+                    aux2.hDer = aux;
                 }
             }
+        }
+    }
+    
+    public Nodo min(){
+        Nodo aux = new Nodo();
+        aux = this.raiz;
+        while(aux.hIzq != null){
+            aux = aux.hIzq;
+        }
+        return aux;
+    }
+    
+    public Nodo max(){
+        Nodo aux = new Nodo();
+        aux = this.raiz;
+        while(aux.hDer != null){
+            aux = aux.hDer;
+        }
+        return aux;
+    }
+    
+    public String InOrder(Nodo aux){
+        String texto = "";
+        if(aux != null){
+            this.InOrder(aux.hIzq);
+            texto = aux.mostrar() + "/n";
+            this.InOrder(aux.hDer);
         }
     }
 }
